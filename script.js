f00a54fb559d4b8a95b0b35976b3d49597046e62
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-05-24 15:53:27 +0500

    Completed

---
+++ script.js
@@ -0,0 +1,27 @@
+ // Question 1
+ var a = 10
+ document.write("Result:")
+ document.write("<br/>","The value of a is : ",a)
+ document.write("<br/>","<b>.........................................</b>")
+ document.write("<br/><br/>","The value of ++a is : ",++a)
+ document.write("<br/>","Now the value of a is : ",a)
+ document.write("<br/><br/>","The value of a++ is : ",a++)
+ document.write("<br/>","Now the value of a is : ",a)
+ document.write("<br/><br/>","The value of --a is : ",--a)
+ document.write("<br/>","Now the value of a is : ",a)
+ document.write("<br/><br/>","The value of a-- is : ",a--)
+ document.write("<br/>","Now the value of a is : ",a)
+
+
+ // Question 2
+ document.write("<br/><hr/>")
+ var a = 2, b = 1
+ document.write("<br/>a is = ",a)
+ document.write("<br/>b is = ",b)
+ var result = --a - --b + ++b + b--
+ document.write("<br/>result is = ",result)
+
+ // Question 3
+ document.write("<br/><hr/>")
+ var name = prompt("Enter your name")
+ document.write(" <br/> <h1>Hello ", name,"</h1>")
